3acfbe011e9d9ecb54a2ffa55836558ce2448edc
aditya <adimeena39@gmail.com>
2022-04-30 17:41:24 +0530

    some array methods

---
+++ Camelize a string/main.js
@@ -0,0 +1,22 @@
+// Write the function camelize(str) that changes dash-separated words like “my-short-string” into camel-cased “myShortString”.
+
+// That is: removes all dashes, each word after dash becomes uppercased.
+
+// Examples:
+
+// camelize("background-color") == 'backgroundColor';
+// camelize("list-style-image") == 'listStyleImage';
+// camelize("-webkit-transition") == 'WebkitTransition';
+
+
+function camelize(str){
+    let arr = str.split("-")
+   let result = arr.map((word,index)=>
+    index===0 ? word : word[0].toUpperCase() + word.slice(1)
+   )
+   return result.join('')
+   
+}
+
+
+console.log( camelize("background-color")) // backgroundColor
