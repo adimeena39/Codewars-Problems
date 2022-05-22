009869a0e0be3a8707de30ab538a1b39d7da32f0
aditya <adimeena39@gmail.com>
2022-05-22 23:45:35 +0530

    7Kyu

---
+++ Row Weight/main.js
@@ -0,0 +1,30 @@
+// // Scenario
+// Several people are standing in a row divided into two teams.
+// The first person goes into team 1, the second goes into team 2, the third goes into team 1, and so on.
+
+// Task
+// Given an array of positive integers (the weights of the people), return a new array/tuple of two integers, where the first one is the total weight of team 1, and the second one is the total weight of team 2.
+
+// Notes
+// Array size is at least 1.
+// All numbers will be positive.
+// Input >> Output Examples
+// rowWeights([13, 27, 49])  ==>  return (62, 27)
+// Explanation:
+// The first element 62 is the total weight of team 1, and the second element 27 is the total weight of team 2.
+
+// rowWeights([50, 60, 70, 80])  ==>  return (120, 140)
+// Explanation:
+// The first element 120 is the total weight of team 1, and the second element 140 is the total weight of team 2.
+
+
+
+function rowWeights(array){
+    //your code here
+    let team1Sum = 0
+    let team2Sum = 0
+    array.forEach((a,i)=>i%2===0 ? team1Sum+=a : team2Sum+=a  )
+    return [team1Sum, team2Sum]
+  }
+
+  console.log(rowWeights([5,10,14]))  // [ 19, 10 ]
